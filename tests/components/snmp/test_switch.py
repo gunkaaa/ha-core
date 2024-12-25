205ea93a6fd0e38f4a5a54b5d4e1e64cc1843aae
Guy Lowe <github@gunk.gen.nz>
2024-12-26 00:35:44 +1300

    expand SNMP switch coverage

--- tests/components/snmp/test_switch.py
+++ tests/components/snmp/test_switch.py
@@ -5,27 +5,33 @@ from unittest.mock import patch
 from pysnmp.proto.rfc1902 import Integer32
 import pytest
 
-from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
-from homeassistant.const import STATE_OFF, STATE_ON, STATE_UNKNOWN
+from homeassistant.components.switch import (
+    DOMAIN as SWITCH_DOMAIN,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+)
+from homeassistant.const import ATTR_ENTITY_ID, STATE_OFF, STATE_ON, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
 
 config = {
     SWITCH_DOMAIN: {
         "platform": "snmp",
-        "host": "192.168.1.32",
-        # ippower-mib::ippoweroutlet1.0
-        "baseoid": "1.3.6.1.4.1.38107.1.3.1.0",
+        "host": "192.0.2.1",
+        # NET-SNMP-EXAMPLES-MIB::netSnmpExampleInteger
+        "baseoid": "1.3.6.1.4.1.8072.2.1.1",
         "payload_on": 1,
         "payload_off": 0,
     },
 }
 
 
-async def test_snmp_integer_switch_off(hass: HomeAssistant) -> None:
-    """Test snmp switch returning int 0 for off."""
+async def test_snmp_integer_switch_state_returns_off(hass: HomeAssistant) -> None:
+    """Test snmp switch returning integer payload_off using SNMPv2c."""
 
-    mock_data = Integer32(0)
+    config[SWITCH_DOMAIN]["version"] = "2c"
+    mock_data = Integer32(config[SWITCH_DOMAIN]["payload_off"])
+
     with patch(
         "homeassistant.components.snmp.switch.getCmd",
         return_value=(None, None, None, [[mock_data]]),
@@ -33,13 +39,16 @@ async def test_snmp_integer_switch_off(hass: HomeAssistant) -> None:
         assert await async_setup_component(hass, SWITCH_DOMAIN, config)
         await hass.async_block_till_done()
         state = hass.states.get("switch.snmp")
-        assert state.state == STATE_OFF
+
+    assert state.state == STATE_OFF
 
 
-async def test_snmp_integer_switch_on(hass: HomeAssistant) -> None:
-    """Test snmp switch returning int 1 for on."""
+async def test_snmp_integer_switch_state_returns_on(hass: HomeAssistant) -> None:
+    """Test snmp switch returning integer payload_on using SNMPv3."""
 
-    mock_data = Integer32(1)
+    config[SWITCH_DOMAIN]["version"] = "3"
+    mock_data = Integer32(config[SWITCH_DOMAIN]["payload_on"])
+
     with patch(
         "homeassistant.components.snmp.switch.getCmd",
         return_value=(None, None, None, [[mock_data]]),
@@ -47,15 +56,17 @@ async def test_snmp_integer_switch_on(hass: HomeAssistant) -> None:
         assert await async_setup_component(hass, SWITCH_DOMAIN, config)
         await hass.async_block_till_done()
         state = hass.states.get("switch.snmp")
-        assert state.state == STATE_ON
+
+    assert state.state == STATE_ON
 
 
-async def test_snmp_integer_switch_unknown(
+async def test_snmp_integer_switch_state_returns_unknown(
     hass: HomeAssistant, caplog: pytest.LogCaptureFixture
 ) -> None:
     """Test snmp switch returning int 3 (not a configured payload) for unknown."""
 
     mock_data = Integer32(3)
+
     with patch(
         "homeassistant.components.snmp.switch.getCmd",
         return_value=(None, None, None, [[mock_data]]),
@@ -63,5 +74,72 @@ async def test_snmp_integer_switch_unknown(
         assert await async_setup_component(hass, SWITCH_DOMAIN, config)
         await hass.async_block_till_done()
         state = hass.states.get("switch.snmp")
-        assert state.state == STATE_UNKNOWN
-        assert "Invalid payload '3' received for entity" in caplog.text
+
+    assert state.state == STATE_UNKNOWN
+    assert "Invalid payload '3' received for entity" in caplog.text
+
+
+async def test_snmp_integer_switch_turn_on(hass: HomeAssistant) -> None:
+    """Test turning on SNMP switch with payload_on configured as integer, returning integer payload_on, no command_payload_on configured."""
+
+    mock_data = Integer32(config[SWITCH_DOMAIN]["payload_on"])
+
+    with patch(
+        "homeassistant.components.snmp.switch.getCmd",
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        assert await async_setup_component(hass, SWITCH_DOMAIN, config)
+        await hass.async_block_till_done()
+
+    with patch(
+        "homeassistant.components.snmp.switch.setCmd",
+        # https://github.com/etingof/pysnmp/blob/master/pysnmp/hlapi/v3arch/asyncio/cmdgen.py#L168
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TURN_ON,
+            {ATTR_ENTITY_ID: "switch.snmp"},
+            blocking=True,
+        )
+
+    with patch(
+        "homeassistant.components.snmp.switch.getCmd",
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        state = hass.states.get("switch.snmp")
+
+    assert state.state == STATE_ON
+
+
+async def test_snmp_integer_switch_turn_off(hass: HomeAssistant) -> None:
+    """Test turning on SNMP switch with payload_on configured as integer, returning integer payload_on, no command_payload_on configured."""
+
+    mock_data = Integer32(config[SWITCH_DOMAIN]["payload_off"])
+
+    with patch(
+        "homeassistant.components.snmp.switch.getCmd",
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        assert await async_setup_component(hass, SWITCH_DOMAIN, config)
+        await hass.async_block_till_done()
+
+    with patch(
+        "homeassistant.components.snmp.switch.setCmd",
+        # https://github.com/etingof/pysnmp/blob/master/pysnmp/hlapi/v3arch/asyncio/cmdgen.py#L168
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        await hass.services.async_call(
+            SWITCH_DOMAIN,
+            SERVICE_TURN_OFF,
+            {ATTR_ENTITY_ID: "switch.snmp"},
+            blocking=True,
+        )
+
+    with patch(
+        "homeassistant.components.snmp.switch.getCmd",
+        return_value=(None, None, None, [[mock_data]]),
+    ):
+        state = hass.states.get("switch.snmp")
+
+    assert state.state == STATE_OFF
